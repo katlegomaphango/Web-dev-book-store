ce044d26a2aef7a6cf17b516978a959517a8c6a6
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-31 16:38:00 +0200

    clean up

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Web_dev_book_store.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
