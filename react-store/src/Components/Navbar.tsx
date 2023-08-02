d804fbcc5c8a4837bff4da54547782382a5c8565
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-02 15:57:34 +0200

    styled nav

--- react-store/src/Components/Navbar.tsx
+++ react-store/src/Components/Navbar.tsx
@@ -1,9 +1,103 @@
+import { Home, LibraryAdd, LibraryBooks, List, MenuBook } from "@mui/icons-material"
+import { AppBar, Box, Button, Container, Menu, MenuItem, Toolbar, Typography, styled } from "@mui/material"
+import { useState } from "react"
 
+
+const MainBox = styled(Box)({
+    flexGrow: 1
+})
+
+const MainContainer = styled(Container)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+})
+
+const LeftBox = styled(Box)({
+    display: 'flex',
+    alignItems: 'center'
+})
+
+const RightBox = styled(Box)({
+    display: 'flex',
+    alignItems: 'center'
+})
 
 const Navbar = () => {
+    const [anchorEl, setAnchorEl] =useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <>
-        <div>nav</div>
+        <MainBox>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <MainContainer maxWidth='lg'>
+                        <LeftBox>
+                            <Button 
+                                variant="text"
+                                startIcon={<LibraryBooks />}
+                                color="inherit"
+                                size="large"
+                            >
+                                <Typography 
+                                    variant="h6"
+                                    component='div'
+                                >
+                                    WeB Dev books
+                            </Typography>
+                            </Button>
+                        </LeftBox>
+                        <RightBox>
+                            <Button
+                                variant="text"
+                                startIcon={<Home />}
+                                color="inherit"
+                                size="large"
+                            >
+                                Home
+                            </Button>
+                            <div>
+                                <Button
+                                    id="menu-button"
+                                    variant="text"
+                                    color="inherit"
+                                    size="large"
+                                    startIcon={<MenuBook />}
+                                    onClick={handleClick}
+                                >
+                                    Books
+                                </Button>
+                                <Menu 
+                                    open={open}
+                                    onClose={handleClose}
+                                    anchorEl={anchorEl}
+                                    MenuListProps={{
+                                        'aria-labelledby': 'menu-button'
+                                    }}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <List sx={{mr: 1}} />
+                                        View All
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <LibraryAdd sx={{mr: 1}} />
+                                        Add book
+                                    </MenuItem>
+                                </Menu>
+                            </div>
+                        </RightBox>
+                    </MainContainer>
+                </Toolbar>
+            </AppBar>
+        </MainBox>
         </>
     )
 }
