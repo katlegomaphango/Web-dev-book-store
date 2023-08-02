02055d92de80eec9a6f97596bc2883a5ee79ff87
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-03 00:20:00 +0200

    fetch dev books from supabase

--- react-store/src/Components/Home.tsx
+++ react-store/src/Components/Home.tsx
@@ -1,8 +1,13 @@
 import { Box, Paper, Typography, styled } from "@mui/material"
 import wallpaper from '/assets/wallpaper.jpg'
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { supabase } from "../supaBaseClient"
 
-const MainBox = styled(Box)({
-    marginTop: 20
+const BodyBox = styled(Box)({
+    backgroundColor: '#9DB2BF',
+    padding: 20,
+    borderRadius: 5
 })
 
 const HeroPaper = styled(Paper)({
@@ -32,10 +37,30 @@ const HeroSub = styled(Typography)({
     fontStyle: 'italic'
 })
 
+const BodyTitle = styled(Typography)({
+    textAlign: 'center'
+})
+
 const Home = () => {
+    const [top3, setTop3] = useState<any>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data, error } = await supabase
+                .from('devBooks')
+                .select('*')
+                .order('date_Added', { ascending: false })
+                .limit(3)
+
+            if(error) throw error
+            setTop3(data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <>
-        <MainBox>
+        <Box mt={2}>
             <HeroPaper>
                 <HeroTitle variant="h3">
                     Web Development Book Store
@@ -44,8 +69,33 @@ const Home = () => {
                     Shop online at the WebDev Book Store for amazing discounts on best-selling web development books!
                 </HeroSub>
             </HeroPaper>
+            <BodyBox mt={2}>
+                <BodyTitle variant="h3">
+                    New Releases
+                </BodyTitle>
+                <Box mt={2}>
+                    {
+                        top3.map((book) => (
+                            <Box key={book.id} mb={3}>
+                                <Link to={'/'} style={{textDecoration: 'none'}}>
+                                    <Typography variant="h5">
+                                        {book.title}
+                                    </Typography>
+                                </Link>
+                                <Typography variant="h6">
+                                    R {book.price}
+                                </Typography>
+                                <Typography sx={{fontStyle: 'italic'}}>
+                                    Written by {book.author} <br />
+                                    Added to collection on {book.date_Added}
+                                </Typography>
+                            </Box>
+                        ))
+                    }
+                </Box>
+            </BodyBox>
             Home
-        </MainBox>
+        </Box>
         </>
     )
 }
