86ee9784d15416d7dd6b93f6af5b7d439f27cd0b
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-07 03:55:45 +0200

    clean up

--- asp.net-store/dotnet-webDev-store/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_webDev_store.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
