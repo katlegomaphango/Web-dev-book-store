4117cfeabececa0d7fc30476aad3275d202d1ecc
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-08 16:13:51 +0200

    book repo

---
+++ asp.net-store/dotnet-webDev-store/Data/BookRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_webDev_store.Models;
+
+namespace dotnet_webDev_store.Data
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BookRepository(AppDbContext app)
+        {
+            _appDbContext = app;
+        }
+
+        public void AddBook(Book book)
+        {
+            _appDbContext.Books.Add(book);
+            _appDbContext.SaveChanges();
+        }
+
+        public IQueryable<Book> GetAllBooks()
+        {
+            return _appDbContext.Books;
+        }
+
+        public Book GetBookById(int id)
+        {
+            return _appDbContext.Books.Find(id);
+        }
+    }
+}
