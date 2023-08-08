9fbc99ba1440317aec449601db1a4556e5322243
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-08 15:48:10 +0200

    null safety

--- asp.net-store/dotnet-webDev-store/Models/Book.cs
+++ asp.net-store/dotnet-webDev-store/Models/Book.cs
@@ -3,9 +3,9 @@ namespace dotnet_webDev_store.Models
     public class Book
     {
         public int BookId { get; set;}
-        public string BookTitle { get; set;}
-        public string Author { get; set;}
-        public string Description { get; set;}
+        public string? BookTitle { get; set;}
+        public string? Author { get; set;}
+        public string? Description { get; set;}
         public decimal Price { get; set;}
         public int Rating { get; set;}
         public DateTime DateAdded { get; set;}
