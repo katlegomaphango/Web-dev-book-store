ffb7ba17ada42c56866eb7e4fb67af33454dbbbc
Katlego Maphango <mphangokatlego08@gmail.com>
2023-08-10 22:03:07 +0200

    all books loading state

--- react-store/src/Components/ViewAllBooks.tsx
+++ react-store/src/Components/ViewAllBooks.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../supaBaseClient"
-import { Box, Rating, Typography } from "@mui/material"
+import { Box, Rating, Skeleton, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
 
 
@@ -22,13 +22,26 @@ const ViewAllBooks = () => {
 
     return (
         <>
-        <Box my={10}>
+        <Box my={10} sx={{backgroundColor: '#9DB2BF', p: 10, borderRadius: 3}}>
             <Typography variant="h3" sx={{textAlign: 'center'}}>
                 Our Current Collection of WebDev Books
             </Typography>
-            <Box mt={2}>
+            <Box mt={7}>
                 {
-                    bookArr.map((book) => (
+                    bookArr.length === 0 ? (
+                        [0,1,2,3,4,5,6,7,8,9].map((_, i) => (
+                            <Box key={i}>
+                                <Box mb={3}> 
+                                    <Skeleton sx={{width: {xs: '100%', md: '60%'}}} height={50} />
+                                    <Skeleton sx={{width: {xs: '40%', md: '20%'}}} height={40} />
+                                    <Skeleton sx={{width: {xs: '100%', md: '40%'}}} height={20} />
+                                    <Skeleton sx={{width: {xs: '100%', md: '40%'}}} height={20} />
+                                    <Skeleton sx={{width: {xs: '40%', md: '20%'}}} height={40} />
+                                </Box>
+                            </Box>
+                        ))
+                    ) :
+                    (bookArr.map((book) => (
                         <Box key={book.id} mb={3}>
                             <Link to={`/ViewBook/${book.book_id}`} style={{textDecoration: 'none'}}>
                                 <Typography variant="h5">
@@ -46,7 +59,7 @@ const ViewAllBooks = () => {
                                 <Rating value={book.rating} readOnly sx={{color: 'black'}} />
                             </Typography>
                         </Box>
-                    ))
+                    )))
                 }
             </Box>
         </Box>
